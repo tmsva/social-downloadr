7ea55c2075be5fcd4ebf01e741c8c1754aafea50
zfx01 <tom.valdenegro@gmail.com>
2020-10-22 13:36:18 -0300

    obtiene url del post para descargar la metadata

--- app/src/main/java/free/thirdpack/instadownloader/MainActivity.kt
+++ app/src/main/java/free/thirdpack/instadownloader/MainActivity.kt
@@ -1,11 +1,26 @@
 package free.thirdpack.instadownloader
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val TYPE_TEXT_PLAIN = "text/plain"
+        const val DOMAIN = "instagram.com"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        intent.run {
+            if (type == TYPE_TEXT_PLAIN && clipData != null) {
+                val url = clipData!!.getItemAt(0).text as String
+                if (url.contains(DOMAIN)) {
+                    val metaUrl = url.replaceAfter("/?", "__a=1")
+                    Log.d("TAK", "meta url:> $metaUrl")
+                }
+            }
+        }
     }
 }
