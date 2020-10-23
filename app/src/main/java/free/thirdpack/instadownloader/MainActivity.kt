d9aa2258bdda013681b2f690b647e3ab474ab3ee
zfx01 <tom.valdenegro@gmail.com>
2020-10-23 19:37:20 -0300

    fix no recibe intents al estar lanzada la app

--- app/src/main/java/free/thirdpack/instadownloader/MainActivity.kt
+++ app/src/main/java/free/thirdpack/instadownloader/MainActivity.kt
@@ -1,5 +1,6 @@
 package free.thirdpack.instadownloader
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
@@ -13,19 +14,22 @@ class MainActivity : AppCompatActivity() {
         const val TYPE_TEXT_PLAIN = "text/plain"
         const val DOMAIN = "instagram.com"
         const val DOWNLOAD_FOLDER = "InstaDownloader"
-        const val BASE_URL = "https://instagram.com/p/"
+        const val BASE_URL = "https://$DOMAIN/p/"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        intent.run {
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        intent?.run {
             if (type == TYPE_TEXT_PLAIN && clipData != null) {
                 val url = clipData!!.getItemAt(0).text as String
                 if (url.contains(DOMAIN)) {
                     val postId = url.split("/")[4]
-                    Log.d("TAK", "post id:> $postId")
+                    Log.d("TAK", "getting meta data")
                     viewModel.getMetaPost(postId)
                 }
             }
